287d0535c12f4da3b06cbb0f88ec8d35dcacd154
yossef-khaled <yossef.k.y333@gmail.com>
2020-09-04 00:25:27 +0200

    Initial commit.

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const http = require('http');
+
+const hostName = 'Localhost';
+const port = 3000;
+
+const app = express();
+
+app.use((req, res, next) => {
+    console.log(req.headers);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end(`<html><body><h1>This is an express server</h1></body></html>`);
+});
+
+const server = http.createServer(app);
+
+server.listen(port, hostName, () => {
+    console.log(`Server is runnind at http://${hostName}:${port}`);
+});
