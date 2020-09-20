ad4c68d0866808c12ed0f63c9821ca27bdd817b6
yossef-khaled <yossef.k.y333@gmail.com>
2020-09-20 15:01:18 +0200

    Impement login with passport.

--- Routes/UsersRouter.js
+++ Routes/UsersRouter.js
@@ -28,52 +28,11 @@ userRouter.post('/signup', (req, res, next) => {
     });
 });
 
-userRouter.post('/login', (req, res, next) => {
-    //Which means the first time to authorize himself.
-    if(!req.session.user) {
-        //authorizationHeader is an encryption of the user name and password 
-        //example : 'basic aefdafferfwd622gfad'  
-        var authorizationHeader = req.headers.authorization;
-        if(!authorizationHeader) {
-            var error = new Error('You are NOT authenticated');
-            error.status = 401;
-            res.setHeader('WWW-Authenticate', 'Basic');
-            return next(error);
-        }
-
-        var userIdentity = new Buffer.from(authorizationHeader.split(' ')[1], 'base64').toString().split(':');
-        var userName = userIdentity[0];
-        var password = userIdentity[1];
-        
-        Users.find({userName : userName})
-        .then((user) => {
-            if(user == null) {
-                var error = new Error(`User ${user} does NOT exist`);
-                error.status = 403;
-                res.setHeader('WWW-Authenticate', 'Basic');
-                return next(error);
-            }
-            else if(user.password !== password) {
-                var error = new Error('Password is INCORRECT');
-                error.status = 403;
-                res.setHeader('WWW-Authenticate', 'Basic');
-                return next(error);
-            }
-            else {
-                req.session.user = 'authenticated';
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'text/plain');
-                res.end('You are authenticated!')
-            }
-        })
-        .catch((error) => next(error));
-    }
-    else {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'plain/text');
-        res.end('You are already authenticated');
-    }
-})
+userRouter.post('/login', passport.authenticate('local'), (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({success : true, status : 'You are successfully logged in'});
+});
 
 userRouter.get('/logout', (req, res, next) => {
     if(req.session) {
