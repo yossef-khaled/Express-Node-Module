4e27f2e21eb2196170373817c2dbcf20ab5335d2
yossef-khaled <yossef.k.y333@gmail.com>
2020-09-22 18:51:01 +0200

    Updated user schema.

--- Models/Users.js
+++ Models/Users.js
@@ -4,6 +4,14 @@ var Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
 
 var User = new Schema({
+    firstname : {
+        type : String,
+        required : true
+    },
+    lastname : {
+        type : String,
+        required : true
+    },
     admin : {
         type : Boolean,
         default : false
